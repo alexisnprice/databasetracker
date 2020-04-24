dd06d59dd42d80dc5dbc1f8e101cbbda79ab66f1
alexisnprice <pricenalexis@gmail.com>
2020-04-23 21:11:27 -0500

    adding data draft

---
+++ tracker-data.sql
@@ -0,0 +1,78 @@
+/************************************************************************/
+/*																		*/
+/*	Alexis Price & Kelly Kang					                        */
+/*																		*/
+/*	4480 Final Project              									*/
+/*																		*/
+/************************************************************************/
+
+INSERT INTO hospital VALUES ('20569','Anchorage','AK');
+INSERT INTO hospital VALUES ('38214','Juneau','AK');
+INSERT INTO hospital VALUES ('00933','Soldotna','AK');
+INSERT INTO hospital VALUES ('16453','Cordova','AK');
+INSERT INTO hospital VALUES ('10696','Fairbanks','AK');
+INSERT INTO hospital VALUES ('52638','Kodiak','AK');
+INSERT INTO hospital VALUES ('14881','Petersburg','AK');
+INSERT INTO hospital VALUES ('NY');
+INSERT INTO hospital VALUES ('NY');
+INSERT INTO hospital VALUES ('NY');
+INSERT INTO hospital VALUES ('NY');
+INSERT INTO hospital VALUES ('NY');
+INSERT INTO hospital VALUES ('NY');
+INSERT INTO hospital VALUES ('NY');
+INSERT INTO hospital VALUES ('NY');
+INSERT INTO hospital VALUES ('NY');
+INSERT INTO hospital VALUES ('NY');
+INSERT INTO hospital VALUES ('NY');
+
+INSERT INTO uscases VALUES ('NY','257246','15302');
+INSERT INTO uscases VALUES ('NJ','95865','5063');
+INSERT INTO uscases VALUES ('MA','42944','2182');
+INSERT INTO uscases VALUES ('CA','37534','1424');
+INSERT INTO uscases VALUES ('PA','36763','1720');
+INSERT INTO uscases VALUES ('IL','35108','1577');
+INSERT INTO uscases VALUES ('MI','33929', '2812');
+INSERT INTO uscases VALUES ('FL','28301','926');
+INSERT INTO uscases VALUES ('LA','25258','1473');
+INSERT INTO uscases VALUES ('CT','22469','1544');
+INSERT INTO uscases VALUES ('TX','21590','569');
+INSERT INTO uscases VALUES ('GA','20099','837');
+INSERT INTO uscases VALUES ('MD','14775','610');
+INSERT INTO uscases VALUES ('OH','14177','610');
+INSERT INTO uscases VALUES ('WA','12539','696');
+INSERT INTO uscases VALUES ('IN','12438','661');
+INSERT INTO uscases VALUES ('CO','10878','521');
+INSERT INTO uscases VALUES ('VA','10266','349');
+INSERT INTO uscases VALUES ('TN','7704','177');
+INSERT INTO uscases VALUES ('NC','7220','242');
+INSERT INTO uscases VALUES ('MO','6137','237');
+INSERT INTO uscases VALUES ('RI','5841','181');
+INSERT INTO uscases VALUES ('AL','5610','201');
+INSERT INTO uscases VALUES ('AZ','5459','236');
+INSERT INTO uscases VALUES ('MS','4894','193');
+INSERT INTO uscases VALUES ('WI','4845','247');
+INSERT INTO uscases VALUES ('SC','4761','140');
+INSERT INTO uscases VALUES ('NV','4081','187');
+INSERT INTO uscases VALUES ('IA','3764','90');
+INSERT INTO uscases VALUES ('UT','3451','34');
+INSERT INTO uscases VALUES ('KY','3373','185');
+INSERT INTO uscases VALUES ('DC','3206','127');
+INSERT INTO uscases VALUES ('DE','3200','89');
+INSERT INTO uscases VALUES ('OK','2894','169');
+INSERT INTO uscases VALUES ('MN','2721','179');
+INSERT INTO uscases VALUES ('AR','2392','44');
+INSERT INTO uscases VALUES ('KS','2292','111');
+INSERT INTO uscases VALUES ('NM','2210','71');
+INSERT INTO uscases VALUES ('OR','2059','78');
+INSERT INTO uscases VALUES ('SD','1858','9');
+INSERT INTO uscases VALUES ('NE','1827','45');
+INSERT INTO uscases VALUES ('ID','1688','54');
+INSERT INTO uscases VALUES ('NH','1588','48');
+INSERT INTO uscases VALUES ('WV','939','46');
+INSERT INTO uscases VALUES ('ME','907','39');
+INSERT INTO uscases VALUES ('VT','823','40');
+INSERT INTO uscases VALUES ('ND','679','14');
+INSERT INTO uscases VALUES ('HI','586','12');
+INSERT INTO uscases VALUES ('MT','439','14');
+INSERT INTO uscases VALUES ('AK','335','9');
+INSERT INTO uscases VALUES ('WY','326','6');
